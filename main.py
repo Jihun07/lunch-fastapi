9bb079e7e42ef1a14772863495cf56102a2eea61
Jihun07 <130033672+Jihun07@users.noreply.github.com>
2025-05-07 18:35:20 +0900

    Update main.py

--- main.py
+++ main.py
@@ -12,7 +12,17 @@ async def get_lunch():
     async with httpx.AsyncClient() as client:
         res = await client.get("https://raw.githubusercontent.com/Jihun07/Lunchcrawler/main/today_menu.json")
         data = res.json()
+
+        # 카카오 스킬 응답 형식으로 리턴
         return {
-            "date": data["date"],
-            "menu": data["menu"]
+            "version": "2.0",
+            "template": {
+                "outputs": [
+                    {
+                        "simpleText": {
+                            "text": f"📅 {data['date']}\n🍱 {data['menu']}"
+                        }
+                    }
+                ]
+            }
         }
