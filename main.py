e30bd4bff1ed3a1102c3ef39d7bc7e46c7ada5f1
Jihun07 <130033672+Jihun07@users.noreply.github.com>
2025-05-07 18:41:16 +0900

    Update main.py

--- main.py
+++ main.py
@@ -3,24 +3,21 @@ import httpx
 
 app = FastAPI()
 
-@app.get("/")
-def root():
-    return {"message": "FastAPI is running."}
-
 @app.get("/lunch")
 async def get_lunch():
     async with httpx.AsyncClient() as client:
         res = await client.get("https://raw.githubusercontent.com/Jihun07/Lunchcrawler/main/today_menu.json")
         data = res.json()
+        date = data["date"]
+        menu = data["menu"]
 
-        # 카카오 스킬 응답 형식으로 리턴
         return {
             "version": "2.0",
             "template": {
                 "outputs": [
                     {
                         "simpleText": {
-                            "text": f"📅 {data['date']}\n🍱 {data['menu']}"
+                            "text": f"📅 {date}\n🍱 {menu}"
                         }
                     }
                 ]
